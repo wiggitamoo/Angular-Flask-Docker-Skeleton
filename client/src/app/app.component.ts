eccced9f08664e8cf5aebad150c9387174afddc1
Andre Henriques <andrec.henriques@gmail.com>
2021-06-06 00:46:44 -0700

    Updates app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {AppService} from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'App!!';
+  title = 'Resource Manager';
   msg: string;
 
   constructor(private appService: AppService) {
